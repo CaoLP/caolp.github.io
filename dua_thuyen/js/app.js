ba082934bfa0a5c2fa4ff02c61076547a253501b
Cao Le <Cao Le>
2016-03-23 16:44:35 +0700

    update dua thuyen

--- dua_thuyen/js/app.js
+++ dua_thuyen/js/app.js
@@ -1,10 +1,23 @@
 $(function () {
-    $('.btn-login').on('click', function(e){
-        e.preventDefault();
-       $('#login-box').show();
-    });
-    $('.login-box-close').on('click', function (e) {
-        e.preventDefault();
-        $('#login-box').hide();
-    });
+	$('.btn-login').on('click', function (e) {
+		e.preventDefault();
+		$('#login-box').show();
+	});
+	$('.login-box-close').on('click', function (e) {
+		e.preventDefault();
+		$('#login-box').hide();
+	});
+	$('.avatar-list li').click(function () {
+		showDes(this);
+	}).hover(function () {
+		showDes(this);
+	});
+	function showDes(e) {
+		var id = $(e).data('id');
+		$('.arrow img').hide();
+		$('.des li').hide();
+
+		$('.arrow-' + id).css('display','inline-block');
+		$('.des-' + id).css('display','inline-block');
+	}
 });
