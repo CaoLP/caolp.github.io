12a0b59b18abb3975dfb69f727d081c19b18845d
caolp <caolpgc60067@gmail.com>
2016-03-26 15:16:10 +0700

    update

--- le_hoi_cong_chieng/js/app.js
+++ le_hoi_cong_chieng/js/app.js
@@ -3,10 +3,22 @@ $(function () {
         $('.modal-overlay').show();
         var target = $(this).data('target');
         $('.modal').hide();
+        if(target == "dang-ky"){
+            $('.should-hide').hide();
+            $('.overlay-dang-ky').show();
+            $.ajax({
+                url: 'form/form.html',
+                success : function(response){
+                    $('#dang-ky .modal-body').html(response);
+                }
+            });
+        }
         $('#' + target).show();
     });
     $('.close-modal-btn').on('click', function () {
         $('.modal-overlay').hide();
+        $('.overlay-dang-ky').hide();
+        $('.should-hide').show();
         $('.modal').hide();
     });
 });
